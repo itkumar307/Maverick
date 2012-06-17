e9c31869002cad1b2887f11fa749d0865adf1237
kvel <kv@scilla.in>
2012-06-17 12:31:09 +0530

    initial

---
+++ src/com/maveric/MavericActivity.java
@@ -0,0 +1,12 @@
+package com.maveric;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MavericActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen2);
+    }
+}
