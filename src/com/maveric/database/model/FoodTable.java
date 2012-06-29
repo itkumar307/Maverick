c44f5dec032522506b61f3c489a79432a1b36b27
Mohan <mohan@gv-govindasamys-macbook.local>
2012-06-29 16:25:16 +0530

    food table missing file added

---
+++ src/com/maveric/database/model/FoodTable.java
@@ -0,0 +1,43 @@
+package com.maveric.database.model;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class FoodTable {
+
+	public static final String TABLE = "Food";
+
+	public static class Column {
+		public static final String ID = "id";
+		public static final String NAME = "name";
+		public static final String PROTIN = "protin";
+		public static final String CARBOS = "carbos";
+		public static final String FAT = "fat";
+		public static final String CALORIES = "calories";
+		public static final String UPDATED = "updated";
+		public static final String CREATED = "created";
+	}
+
+	public static String[] getColumns() {
+		String[] columns = { Column.ID, Column.NAME, Column.PROTIN,
+				Column.CARBOS, Column.FAT, Column.CALORIES, };
+		return columns;
+	}
+
+	// Database creation SQL statement
+	private static final String DATABASE_CREATE = "CREATE TABLE " + TABLE
+			+ " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " + Column.ID
+			+ " TEXT UNIQUE, " + Column.NAME + " TEXT," + Column.PROTIN
+			+ " TEXT," + Column.CARBOS + " TEXT," + Column.FAT + " TEXT, "
+			+ Column.CREATED + " INTEGER,"+ Column.UPDATED + " INTEGER,"+ Column.CALORIES + " TEXT ); ";
+
+	public static void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	public static void onUpgrade(SQLiteDatabase database, int oldVersion,
+			int newVersion) {
+
+		// TODO Version check has to done here to avoid old version database
+		// crack while upgrading new version database
+	}
+}
